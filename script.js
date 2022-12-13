d3fc7e1b35306e6617151a97acbc82fd02146a17
Mummuro <105568904+Mummuro@users.noreply.github.com>
2022-12-13 03:08:38 -0500

    Declaring main variables #04 Modified CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+const weaponsButtons = document.querySelectorAll('.weapon-button');
+const rounds = document.querySelector('.round');
+const combatText = document.querySelector('.combat-text');
+const buttonPlayAgain = document.querySelector('.play-again');
+
+let playerLives = 5;
+let computerLives = 5;
+let round = 0;
